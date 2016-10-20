b4b2517c0e74eae67087270ada2a9a3b57757a09
novacrazy <novacrazy@gmail.com>
2016-10-19 19:46:53 -0500

    Add `aspect_ratio`, `no_error` and `refresh_rate` helpers, and simplified some code in the window creation function.

--- src/lib.rs
+++ src/lib.rs
@@ -16,14 +16,11 @@
 //! Builder for a GLFW window with robust OpenGL context selection. See the `WindowBuilder` type.
 
 extern crate glfw;
-#[macro_use]
-extern crate log;
+#[macro_use] extern crate log;
 
 use std::sync::mpsc::Receiver;
 
 use glfw::{Glfw, WindowMode, WindowHint, Window, WindowEvent, OpenGlProfileHint};
-use glfw::WindowMode::Windowed;
-use glfw::WindowHint::{ContextVersion, OpenGlForwardCompat, OpenGlProfile};
 
 /// Builder for a GLFW window with robust OpenGL context selection.
 ///
@@ -52,6 +49,7 @@ use glfw::WindowHint::{ContextVersion, OpenGlForwardCompat, OpenGlProfile};
 pub struct WindowBuilder<'glfw, 'title, 'monitor> {
     glfw: &'glfw mut Glfw,
     size: Option<(u32, u32)>,
+    aspect_ratio: Option<(u32, u32)>,
     title: Option<&'title str>,
     mode: Option<WindowMode<'monitor>>,
     common_hints: Vec<WindowHint>,
@@ -64,6 +62,7 @@ impl<'glfw, 'title, 'monitor> WindowBuilder<'glfw, 'title, 'monitor> {
         WindowBuilder {
             glfw: glfw,
             size: None,
+            aspect_ratio: None,
             title: None,
             mode: None,
             try_hints: vec![],
@@ -75,26 +74,38 @@ impl<'glfw, 'title, 'monitor> WindowBuilder<'glfw, 'title, 'monitor> {
 impl<'glfw, 'title, 'monitor, 'hints> WindowBuilder<'glfw, 'title, 'monitor> {
     /// Sets the size of the GLFW window to `width x height`.
     /// Defaults to `640 x 480` if not given.
-    pub fn size(mut self, width: u32, height: u32)
-    -> WindowBuilder<'glfw, 'title, 'monitor> {
+    pub fn size(mut self, width: u32, height: u32) -> WindowBuilder<'glfw, 'title, 'monitor> {
         self.size = Some((width, height));
         self
     }
 
     /// Sets the title of the GLFW window to `title`.
     /// Defaults to `"GLFW Window"` if not given.
-    pub fn title(mut self, title: &'title str)
-    -> WindowBuilder<'glfw, 'title, 'monitor> {
+    pub fn title(mut self, title: &'title str) -> WindowBuilder<'glfw, 'title, 'monitor> {
         self.title = Some(title);
         self
     }
 
     /// Sets the mode of the GLFW window to `mode`.
     /// Defaults to `Windowed` if not given.
-    pub fn mode(mut self, mode: WindowMode<'monitor>)
-    -> WindowBuilder<'glfw, 'title, 'monitor> {
+    pub fn mode(mut self, mode: WindowMode<'monitor>) -> WindowBuilder<'glfw, 'title, 'monitor> {
         self.mode = Some(mode);
         self
+    }
+
+    /// Tell the OpenGL context that it can expect no errors from your program
+    pub fn no_error(self) -> WindowBuilder<'glfw, 'title, 'monitor> {
+        self.common_hints(&[
+            WindowHint::ContextNoError(true)
+        ])
+    }
+
+    /// Set the desired refresh rate of the GLFW window. If set to `None`,
+    /// it will try for the highest refresh rate possible
+    pub fn refresh_rate(self, rate: Option<u32>) -> WindowBuilder<'glfw, 'title, 'monitor> {
+        self.common_hints(&[
+            WindowHint::RefreshRate(rate)
+        ])
     }
 
     /// Adds a list of `WindowHint`s to try creating a window with.
@@ -105,9 +116,15 @@ impl<'glfw, 'title, 'monitor, 'hints> WindowBuilder<'glfw, 'title, 'monitor> {
     /// This method works in combination with `common_hints()` to create
     /// a list of fallback window configurations to try initializing with.
     /// For details, see `create()`.
-    pub fn try_hints(mut self, hints: &[WindowHint])
-    -> WindowBuilder<'glfw, 'title, 'monitor> {
+    pub fn try_hints(mut self, hints: &[WindowHint]) -> WindowBuilder<'glfw, 'title, 'monitor> {
         self.try_hints.push(hints.iter().map(|&x| x).collect());
+        self
+    }
+
+    /// Sets the aspect ratio of the window.
+    /// It will try to constrain the dimensions to this ratio even when resizing the window
+    pub fn aspect_ratio(mut self, numer: u32, denom: u32) -> WindowBuilder<'glfw, 'title, 'monitor> {
+        self.aspect_ratio = Some((numer, denom));
         self
     }
 
@@ -119,8 +136,7 @@ impl<'glfw, 'title, 'monitor, 'hints> WindowBuilder<'glfw, 'title, 'monitor> {
     /// This method works in combination with `try_hints()` to create
     /// a list of fallback window configurations to try initializing with.
     /// For details, see `create()`.
-    pub fn common_hints(mut self, hints: &[WindowHint])
-    -> WindowBuilder<'glfw, 'title, 'monitor> {
+    pub fn common_hints(mut self, hints: &[WindowHint]) -> WindowBuilder<'glfw, 'title, 'monitor> {
         self.common_hints.extend(hints.iter().map(|&x| x));
         self
     }
@@ -139,64 +155,63 @@ impl<'glfw, 'title, 'monitor, 'hints> WindowBuilder<'glfw, 'title, 'monitor> {
     /// supports newer OpenGL feature under OS X, as 10.7+ supports OpenGL
     /// 3.2 but defaults to a 2.1 context that does _not_ expose the additional
     /// extensions.
-    pub fn try_modern_context_hints(self)
-    -> WindowBuilder<'glfw, 'title, 'monitor> {
+    pub fn try_modern_context_hints(self) -> WindowBuilder<'glfw, 'title, 'monitor> {
         // OS X requires forward compatability, annoyingly enough.
         self.try_hints(&[
             WindowHint::ContextVersion(4, 5),
             WindowHint::OpenGlForwardCompat(true),
             WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
         ])
-        .try_hints(&[
-            WindowHint::ContextVersion(4, 4),
-            WindowHint::OpenGlForwardCompat(true),
-            WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(4, 3),
-            WindowHint::OpenGlForwardCompat(true),
-            WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(4, 2),
-            WindowHint::OpenGlForwardCompat(true),
-            WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(4, 1),
-            WindowHint::OpenGlForwardCompat(true),
-            WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(4, 0),
-            WindowHint::OpenGlForwardCompat(true),
-            WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(3, 2),
-            WindowHint::OpenGlForwardCompat(true),
-            WindowHint::OpenGlProfile(OpenGlProfileHint::Core),
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(3, 1),
-            WindowHint::OpenGlForwardCompat(true),
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(3, 1),
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(3, 0),
-            WindowHint::OpenGlForwardCompat(true),
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(3, 0),
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(2, 1),
-        ])
-        .try_hints(&[
-            WindowHint::ContextVersion(2, 0),
-        ])
+            .try_hints(&[
+                WindowHint::ContextVersion(4, 4),
+                WindowHint::OpenGlForwardCompat(true),
+                WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(4, 3),
+                WindowHint::OpenGlForwardCompat(true),
+                WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(4, 2),
+                WindowHint::OpenGlForwardCompat(true),
+                WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(4, 1),
+                WindowHint::OpenGlForwardCompat(true),
+                WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(4, 0),
+                WindowHint::OpenGlForwardCompat(true),
+                WindowHint::OpenGlProfile(OpenGlProfileHint::Core)
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(3, 2),
+                WindowHint::OpenGlForwardCompat(true),
+                WindowHint::OpenGlProfile(OpenGlProfileHint::Core),
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(3, 1),
+                WindowHint::OpenGlForwardCompat(true),
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(3, 1),
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(3, 0),
+                WindowHint::OpenGlForwardCompat(true),
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(3, 0),
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(2, 1),
+            ])
+            .try_hints(&[
+                WindowHint::ContextVersion(2, 0),
+            ])
     }
 
     /// Try to create the window.
@@ -215,30 +230,33 @@ impl<'glfw, 'title, 'monitor, 'hints> WindowBuilder<'glfw, 'title, 'monitor> {
     ///   (or default values).
     /// - Returns on successful window creation.
     pub fn create(self) -> Option<(Window, Receiver<(f64, WindowEvent)>)> {
-        let WindowBuilder { glfw, common_hints, try_hints, size, title, mode } = self;
+        let WindowBuilder { glfw, common_hints, try_hints, size, aspect_ratio, title, mode } = self;
 
         let (width, height) = size.unwrap_or((640, 480));
         let title = title.unwrap_or("GLFW Window");
         let mode = mode.unwrap_or(WindowMode::Windowed);
 
-        glfw.set_error_callback::<()>(None);
         for setup in try_hints.iter() {
             glfw.default_window_hints();
+
             for hint in common_hints.iter() {
                 glfw.window_hint(*hint);
             }
+
             for hint in setup.iter() {
                 glfw.window_hint(*hint);
             }
-            let r = glfw.create_window(width, height, title, mode);
-            match r {
-                Some((window, events)) => {
-                    info!("Created GLFW window with GL context hints {:?} and {:?}", common_hints, setup);
-                    return Some((window, events));
-                },
-                None => {
-                    debug!("Couldn't create a context for hints {:?} and {:?}", common_hints, setup);
+
+            if let Some((mut window, events)) = glfw.create_window(width, height, title, mode) {
+                info!("Created GLFW window with GL context hints {:?} and {:?}", common_hints, setup);
+
+                if let Some((numer, denom)) = aspect_ratio {
+                    window.set_aspect_ratio(numer, denom);
                 }
+
+                return Some((window, events));
+            } else {
+                debug!("Couldn't create a context for hints {:?} and {:?}", common_hints, setup);
             }
         }
         None
